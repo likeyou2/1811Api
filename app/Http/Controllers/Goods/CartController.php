9ad095b1260c985ea542a6e9134c35fc2e312396
ceshi <shuai@localhost.localdomain>
2019-07-21 08:22:52 +0000

    cartData

--- app/Http/Controllers/Goods/CartController.php
+++ app/Http/Controllers/Goods/CartController.php
@@ -86,7 +86,8 @@ class CartController extends Controller
             ];
             $cartData = CartModel::where($where)->get();
             if(count($cartData)){
-                $cartData = CartModel::where($where)->get()->toArray();
+                $cartData = CartModel::join('goods','goods.goods_id','=','shop_cart.goods_id')->where(['user_id'=>$token['id'],'cart_status'=>1])->first();
+                var_dump($cartData);die;
                 $success = [
                     'code' => 200,
                     'msg' => 'success',
